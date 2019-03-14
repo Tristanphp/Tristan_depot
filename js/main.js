43d03f73908b47c1fbdc7d4befafa9bb6e6d7351
Le Roy Tristan <21704859t>
2019-03-14 11:00:19 +0100

    Objet

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ let tasks = new CompoTaskList({
     element: document.getElementById('tasks'),
     taskList: list
 });
+let choix1 = new Task('Choix_1', null);
+let choix2 = new Task('Choix_2', null);
+let choix3 = new Task('Choix_3', null);
+list.add(choix1);
+list.add(choix2);
+list.add(choix3);
+
+choix1.render();
